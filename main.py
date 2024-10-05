8867d9873febb12a4a91490d27b1ae4fee8f10af
FATBOY123456789 <ytzjimbuisness@gmail.com>
2024-10-05 21:49:46 +0530

    commit 2

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ l1=[52,13,43,51,69]
 l2=[11,12,60,43,20]
 result=map(lambda x,y: x+y, l1,l2)
 print('Addition of the lists are:',list(result))
+l3=[6,1,2,3,4]
+l4=['anuradha','aakash','samudra','haigrev','avyukt']
+l5=zip(l3,l4)
+print(list(l5))
